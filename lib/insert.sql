368dd5eab5d4fddc15bdfe9bc8814f27ab43d645
Peyton Meredith <peymeredith@gmail.com>
2023-11-02 10:57:44 -0500

    bears lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 12, 'M', 'Brown', 'Lazy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 6, 'M', 'Black', 'Rowdy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 28, 'M', 'Orange', 'Sweet', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 17, 'F', 'Brown', 'Bossy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 11, 'F', 'Black and White', 'Aloof', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 32, 'M', 'Brown', 'Mischevious', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 12, 'F', 'White', 'Shy', 4);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 14, 'M', 'Black', 'Sweet', 1);
